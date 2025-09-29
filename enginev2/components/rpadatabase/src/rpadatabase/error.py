86e5771632b4c52e3875f40cecc4382520bef3ba
ybcao5 <ybcao5@iflytek.com>
2025-09-29 19:57:05 +0800

    feat: add atomic actions

---
+++ enginev2/components/rpadatabase/src/rpadatabase/error.py
@@ -0,0 +1,6 @@
+from astronverse.baseline import _
+from astronverse.baseline.error.error import BaseException, BizCode, ErrorCode
+
+BaseException = BaseException
+
+MSG_EMPTY_FORMAT: ErrorCode = ErrorCode(BizCode.LocalErr, _("消息为空") + ": {}")
